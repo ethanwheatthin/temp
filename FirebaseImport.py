ab293662aeb65b4191f4100979f189fcb0f545e1
Ethan Collins <ethan.collins335@myci.csuci.edu>
2020-03-29 23:28:43 -0700

    Editted Step Sheet

--- FirebaseImport.py
+++ FirebaseImport.py
@@ -1,6 +1,42 @@
-from firebase import firebase
+from random import randint
 
-firebase = firebase.FirebaseApplication('https://linedance-f12c4.firebaseio.com/Dance_Details', None)
-result = firebase.get('/Dance_Details', None)
-print (result)
-#{'1': 'John Doe', '2': 'Jane Doe'}
+import firebase_admin
+from firebase_admin import db
+
+from firebase_admin import credentials
+
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
+
+
+if __name__ == "__main__":
+    usedKeys = open('UsedKeys.txt', 'a')
+    usedKeysList = []
+    cred = credentials.Certificate("/Users/EthanCC/Downloads/linedance-f12c4-firebase-adminsdk-aizzs-a0fcca8688.json")
+    #firebase_admin.initialize_app(cred)
+    # Initialize the app with a service account, granting admin privileges
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://linedance-f12c4.firebaseio.com/'
+    })
+
+    # As an admin, the app has access to read and write all data, regradless of Security Rules
+    counter = 1
+    for i in range(3):
+        newKey = random_with_N_digits(5)
+        usedKeysList.append(newKey)
+
+        if not newKey not in usedKeysList:
+            usedKeys.write(str(newKey) + "\n")
+            ref = db.reference('Dance_Details').child("DanceID_"+str(newKey))
+            ref.set({
+                'Dance_Name': 'TEST NEW DANCE NAME ' + str(counter),
+                'Dance_Difficulty': 'TEST MEDIUM DIFFICULTY ' + str(counter),
+                'Dance_Music': 'NEW test music' + str(counter),
+
+            })
+            counter += 1
+            print(ref.get())
+        else:
+            newKey = random_with_N_digits(5)
--- test.py
+++ test.py
@@ -5,22 +5,48 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
 import os.path
-import pymysql
+from random import randint
+
+import firebase_admin
+from firebase_admin import db
+
+from firebase_admin import credentials
+
+
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
+
+
+#def get_new_key()
 
 if __name__ == "__main__":
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    driver = Chrome("C:\\Users\\Ethan\\Desktop\\LineDance\\chromedriver.exe")
+    driver = Chrome("/Users/EthanCC/Desktop/temp/chromedriver")
     link = "https://www.copperknob.co.uk/stepsheets/american-kids-ID98907.aspx"
     # driver.get("https://www.copperknob.co.uk/search.aspx?Order=Alphabetical&Lang=en&SearchType=Title&Level=Beginner&Beat=-1&Wall=-1&Search=&recnum=20")
 
     # driver.get("https://www.copperknob.co.uk/search.aspx?Order=Alphabetical&Lang=en&SearchType=Title&Level=Beginner&Beat=-1&Wall=-1&Search=&recnum=0")
     # title = driver.find_elements_by_xpath("//a[@href]")
-    save_path = "C:\\Users\\Ethan\\Desktop\\MasterLineDances\\BeginnerLineDances"
-    dance_links = open("C:\\Users\\Ethan\\Desktop\\LineDance\\BeginnerDanceLinks.txt")
-    os.chdir(save_path)
-    key = 0
+    #save_path = "C:\\Users\\Ethan\\Desktop\\MasterLineDances\\BeginnerLineDances"
+    dance_links = open("/Users/EthanCC/Desktop/temp/BeginnerDanceLinks.txt")
+
+    usedKeys = open('UsedKeys.txt', 'a')
+    usedKeysList = []
+    cred = credentials.Certificate("/Users/EthanCC/Downloads/linedance-f12c4-firebase-adminsdk-aizzs-a0fcca8688.json")
+    # firebase_admin.initialize_app(cred)
+    # Initialize the app with a service account, granting admin privileges
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://linedance-f12c4.firebaseio.com/'
+    })
+
+    #os.chdir(save_path)
+    counter = 0
+
     for line in dance_links:
+
         try:
             data = {}
             # driver = Chrome("/Users/EthanCC/Desktop/LineDance/chromedriver",chrome_options=chrome_options)
@@ -34,19 +60,24 @@ if __name__ == "__main__":
             dance_choreographer = str(driver.find_element_by_xpath("/html/body/div[5]/div[1]/div[3]/div[3]/div[4]").text).split(':')[1].replace("\n","")
             dance_music = driver.find_element_by_xpath("/html/body/div[5]/div[1]/div[3]/div[3]/div[5]/span").text
             dance_sheet = driver.find_element_by_xpath("/html/body/div[5]/div[1]/div[3]/div[4]/div[1]").text
-            data['Dance_ID'] = key
-            data['Dance_Name'] = dance_name
-            data['Dance_Count'] = dance_count
-            data['Dance_Walls'] = dance_walls
-            data['Dance_Difficulty'] = dance_difficulty
-            data['Dance_Choreographer'] = dance_choreographer
-            data['Dance_Music'] = dance_music
-            data['Dance_Steps'] = dance_sheet
 
-            with open("tempfile" + str(key) + ".json", "w") as outFile:
-                json.dump(data,outFile)
-            key += 1
-            print("Finished Link: " + line)
+            ref = db.reference('Dance_Details').child("DanceID_" + str(counter))
+            ref.set({
+                "Dance_Name": dance_name,
+                "Dance_Count": dance_count,
+                "Dance_Walls": dance_walls,
+                "Dance_Difficulty": dance_difficulty,
+                "Dance_Choreographer": dance_choreographer,
+                "Dance_Music": dance_music,
+                "Dance_Steps": dance_sheet
 
+            })
+
+            counter += 1
+            print(str(counter) + ": ")
+            print(ref.get())
         except:
-            print("Error on link: " + line)
+            print("Error on this link: " + line)
+            break
+
+
